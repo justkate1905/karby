8bf0088ce700e2202fe3b9eb52e156578308c6b7
Ekaterina <cheburashka1997@gmail.com>
2016-04-23 01:24:33 +0300

    Current ver of VM

---
+++ karby.mk
@@ -0,0 +1,112 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Release
+ProjectName            :=karby
+ConfigurationName      :=Release
+WorkspacePath          :=D:/CodeLite/Workspace
+ProjectPath            :=D:/CodeLite/Workspace/karby
+IntermediateDirectory  :=./Release
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=Katerina
+Date                   :=21.04.2016
+CodeLitePath           :="C:/Program Files/CodeLite"
+LinkerName             :=C:/MinGW/bin/g++.exe
+SharedObjectLinkerName :=C:/MinGW/bin/g++.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=$(PreprocessorSwitch)NDEBUG 
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="karby.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=makedir
+RcCmpOptions           := 
+RcCompilerName         :=C:/MinGW/bin/windres.exe
+LinkOptions            :=  
+IncludePath            := $(IncludeSwitch)C:/boost_1_53_0/  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                :=$(LibraryPathSwitch)C:/boost_1_53_0/  $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := C:/MinGW/bin/ar.exe rcu
+CXX      := C:/MinGW/bin/g++.exe
+CC       := C:/MinGW/bin/gcc.exe
+CXXFLAGS :=  -O2 -Wall $(Preprocessors)
+CFLAGS   :=  -O2 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := C:/MinGW/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=C:\Program Files\CodeLite
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "./Release"
+
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "./Release"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/CodeLite/Workspace/karby/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM "main.cpp"
+
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) "main.cpp"
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Release/
+
+
---
+++ main.cpp
@@ -0,0 +1,313 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#define size_opcode int
+using namespace std;
+int main(int argc, char **argv)
+{
+		FILE *file;
+		char* name = " ";
+		vector<double> opcodes;
+		int buff[1];
+		int size, addr_reg1, addr_reg2;
+		short int s;
+		char f;
+		double op1, op2;
+		vector<double> math_stack;
+		if(argc==1){
+			cout<<"There's no filename"<<endl;
+			}
+		else{
+		name = argv[1];
+		}
+		int p;
+		file = fopen(name, "rb");
+		if((file = fopen(name, "rb"))==NULL){
+			cout<< "This file is not exist!"<<endl;
+			}
+		else{
+			fread(buff, sizeof(int), 1, file);
+			size = buff[0];
+			opcodes.push_back(buff[0]);
+			for(int i =0; i < size/4-1;i++){
+				fread(buff, sizeof(int), 1, file);	
+				opcodes.push_back(buff[0]);				
+			}
+		}
+		fclose(file);
+		for(int i =0; i<opcodes.size(); i++){
+			cout<<hex<<opcodes.at(i)<<endl;
+			}
+		for(int i =0; i<opcodes.size(); i++){
+			p=(int)(opcodes.at(i));
+			switch(p){
+				case 0x00A1:
+					break;
+				case 0x00A6:
+					break;
+				case 0x00A7:
+					break;
+				case 0x00A9:
+					break;
+				case 0x00AA:
+					break;
+				case 0x00AB:
+					break;
+				case 0x00AC:
+					break;
+				case 0x00AF:
+					break;
+				case 0x00B0:
+					break;
+				case 0x00C9:
+					cout<<opcodes.at(i+1)<<endl;
+					break;
+				case 0x00CA:
+					//f = ((int)(opcodes.at(i+1))|0)&&0xFFFF;
+					s = ((int)(opcodes.at(i+1))); 
+					//s = 0x00000062;// && 0xFFFF;
+					//cout << "hello"<<endl;
+					cout<< s <<' ' << 16<<endl;
+					break;
+				case 0x00C8:
+					cout<<opcodes.at((int)(opcodes.at(i+1))/sizeof(int)+2)<<endl;
+					break;
+				//Операция сложения	
+				case 0x1000:
+					op1 = opcodes.at((int)(opcodes.at(i+1))/sizeof(int)+2);
+					op2 = opcodes.at((int)(opcodes.at(i+2))/sizeof(int)+2);
+					math_stack.push_back(op1+op2);
+					i+=2;
+					break;
+				case 0x1001:
+					op1 = opcodes.at((int)(opcodes.at(i+1))/sizeof(int)+2);
+					op2 = opcodes.at(i+2);
+					switch((int)(op2)){
+						case 0x00A9:
+							
+							break;
+						case 0x00AA:
+							op2 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1+op2);
+					i+=2;
+					break;
+				case 0x1002:
+					op1 = opcodes.at(i+1);
+					op2 = opcodes.at((int)(opcodes.at(i+2))/sizeof(int)+2);
+					switch((int)(op1)){
+						case 0x00A9:
+							
+							break;
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1+op2);
+					i+=2;
+					break;
+				case 0x1003:
+					op1 = opcodes.at(i+1);
+					op2 = opcodes.at(i+2);
+					switch((int)(op1)){
+						case 0x00A9:
+							op2 = math_stack.back();
+							math_stack.pop_back();
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1+op2);
+					i+=2;
+					break;
+				//Операция вычитания 
+				case 0x1004:
+					op1 = opcodes.at((int)(opcodes.at(i+1))/sizeof(int)+2);
+					op2 = opcodes.at((int)(opcodes.at(i+2))/sizeof(int)+2);
+					math_stack.push_back(op1-op2);
+					i+=2;
+					break;
+				case 0x1005:
+					op1 = opcodes.at((int)(opcodes.at(i+1))/sizeof(int)+2);
+					op2 = opcodes.at(i+2);
+					switch((int)(op2)){
+						case 0x00A9:
+							
+							break;
+						case 0x00AA:
+							op2 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1-op2);
+					i+=2;
+					break;
+				case 0x1006:
+					op1 = opcodes.at(i+1);
+					op2 = opcodes.at((int)(opcodes.at(i+2))/sizeof(int)+2);
+					switch((int)(op1)){
+						case 0x00A9:
+							
+							break;
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1-op2);
+					i+=2;
+					break;
+				case 0x1007:
+					op1 = opcodes.at(i+1);
+					op2 = opcodes.at(i+2);
+					switch((int)(op1)){
+						case 0x00A9:
+							op2 = math_stack.back();
+							math_stack.pop_back();
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1-op2);
+					i+=2;
+					break;
+				//операция умножения	
+				case 0x1008:
+					op1 = opcodes.at((int)(opcodes.at(i+1))/sizeof(int)+2);
+					op2 = opcodes.at((int)(opcodes.at(i+2))/sizeof(int)+2);
+					math_stack.push_back(op1*op2);
+					i+=2;
+					break;
+				case 0x1009:
+					op1 = opcodes.at(opcodes.at(i+1)/sizeof(int)+2);
+					op2 = opcodes.at(i+2);
+					switch((int)(op2)){
+						case 0x00A9:
+							
+							break;
+						case 0x00AA:
+							op2 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1*op2);
+					i+=2;
+					break;
+				case 0x100A:
+					op1 = opcodes.at(i+1);
+					op2 = opcodes.at(opcodes.at(i+2)/sizeof(int)+2);
+					switch((int)(op1)){
+						case 0x00A9:
+							
+							break;
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1*op2);
+					i+=2;
+					break;
+				case 0x100B:
+					op1 = opcodes.at(i+1);
+					op2 = opcodes.at(i+2);
+					switch((int)(op1)){
+						case 0x00A9:
+							op2 = math_stack.back();
+							math_stack.pop_back();
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back(op1*op2);
+					i+=2;
+					break;
+				//Операция деления
+				case 0x100C:
+					op1 = opcodes.at((int)(opcodes.at(i+1))/sizeof(int)+2);
+					op2 = opcodes.at((int)(opcodes.at(i+2))/sizeof(int)+2);
+					math_stack.push_back((double)(op1)/(double)(op2));
+					i+=2;
+					break;
+				case 0x100D:
+					op1 = opcodes.at((int)(opcodes.at(i+1))/sizeof(int)+2);
+					op2 = opcodes.at(i+2);
+					switch((int)(op2)){
+						case 0x00A9:
+							
+							break;
+						case 0x00AA:
+							op2 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back((double)(op1)/(double)(op2));
+					i+=2;
+					break;
+					break;
+				case 0x100E:
+					op1 = opcodes.at(i+1);
+					op2 = opcodes.at((int)(opcodes.at(i+2))/sizeof(int)+2);
+					switch((int)(op1)){
+						case 0x00A9:
+							
+							break;
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back((double)(op1)/(double)(op2));
+					i+=2;
+					break;
+				case 0x100F:
+					op1 = opcodes.at(i+1);
+					op2 = opcodes.at(i+2);
+					switch((int)(op1)){
+						case 0x00A9:
+							op2 = math_stack.back();
+							math_stack.pop_back();
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+							break;
+					} 
+					math_stack.push_back((double)(op1)/(double)(op2));
+					i+=2;
+					break;
+				case 0x3000:
+					opcodes.at(opcodes.at(i+1)/sizeof(int)+2) = opcodes.at(opcodes.at(i+2)/sizeof(int)+2);
+					break;
+				case 0x3001:
+					//cout<<dec<<"~~~~"<<math_stack.back()<<"~~~~~"<<endl;
+					op1 =  opcodes.at(i+2);
+					switch((int)(op1)){
+						case 0x00AA:
+							op1 = math_stack.back();
+							math_stack.pop_back();
+						break;
+					}
+					opcodes.at(opcodes.at(i+1)/sizeof(int)+2) = op1;
+					break;
+			}
+		}
+	return 0;
+}
